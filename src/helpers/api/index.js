4a6414ebf9c8e120a09cc1c312b58dbc271a40c3
YasinHeidari <yasin.gh.heidari@gmail.com>
2024-02-22 13:40:28 +0330

    added the api

--- src/helpers/api/index.js
+++ src/helpers/api/index.js
@@ -1,5 +1,15 @@
-import axios from "axios";
-export const api = axios.create({
-    baseURL: "",
-});
-export default api ;
+const axios = require('axios');
+
+const token = '4fba95dbf46cd77d415830c228c9ef01';
+
+axios.get('https://api.themoviedb.org/3/movie/', { 
+    headers: {
+      'Authorization': 'Bearer ' + token
+    } 
+  })
+  .then(response => {
+    // Handle success
+  })
+  .catch(error => {
+    // Handle error
+  });
