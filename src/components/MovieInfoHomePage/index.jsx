2cc06ad02fb60f653716d49d5a16b2d1fcc03980
YasinHeidari <yasin.gh.heidari@gmail.com>
2024-04-06 23:06:57 +0330

    added some adjusments to the MovieInfoHomePage component

--- src/components/MovieInfoHomePage/index.jsx
+++ src/components/MovieInfoHomePage/index.jsx
@@ -1,54 +1,62 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import ratingDecimal from "../../helpers/ratingdecimal";
-import "./index.css";
+import "./info.css";
 
-const MovieInfoHomePage = ({ movie }) => {
-  const [expanded, setExpanded] = useState(false);
-  const buttonRef = useRef(null);
+const MovieInfoHomePage = ({ movie, buttonContent }) => {
+  const [expanded, setExpanded] = useState(true); // Initially expanded
+
+  useEffect(() => {
+    // Reset expanded state when movie prop changes
+    setExpanded(true);
+  }, [movie]);
 
   const toggleHeight = () => {
     setExpanded(prevExpanded => !prevExpanded); // Toggle the expanded state
     console.log('Button clicked. Expanded:', !expanded);
   };
 
-  console.log('Expanded:', expanded); // Log the current value of expanded
-
   if (!movie) {
     return null;
   }
 
   return (
-    <div className={`container containerMovieInfo ${expanded ? "expanded" : ""}`}>
-      <div className="movieInfoContainer d-flex flex-column justify-center align-start container-padding-2 w-100" style={{background:`url(${movie.poster_path}) center cover`,}}>
-        <Link>{}</Link>
-        <div className=" w-100 h-100 d-flex  justify-evenly align-start gap-2">
-          <div className="d-flex justify-center align-center">
-            <div>
-              <img src={require("../../images/IMDB.svg").default} alt="" />
+    <div>
+      {expanded && (
+        <div className="containerMovieInfo" style={{ background: `url(https://image.tmdb.org/t/p/w300//${movie.backdrop_path}) left/cover` }}>
+          <div className=" d-flex flex-column justify-center align-start gap-2 container-padding-2 col-6 h-100" style={{}} >
+                <Link to="/" className="font-20 white-color">{movie.title || movie.name}</Link>
+            
+            <div className="d-flex justify-start align-center gap-2">
+              <div className="d-flex justify-center align-center">
+                <div>
+                  <img src={require("../../images/IMDB.svg").default} alt="" />
+                </div>
+                <p className="white-color font-14">
+                  {ratingDecimal(movie.vote_average)}
+                </p>
+              </div>
+              <div className="d-flex justify-center align-center">
+                <div>
+                  <img src={require("../../images/subScript.svg").default} alt="" />
+                </div>
+                <p className="white-color font-12"> زیرنویس </p>
+              </div>
+              <div className="d-flex justify-center align-center">
+                <p className="white-color font-12">
+                  { movie.release_date.substring(0, 4)}
+                </p>
+              </div>
             </div>
-            <p className="white-color font-14">
-              {movie.vote_average ? ratingDecimal(movie.vote_average) : "N/A"}
-            </p>
-          </div>
-          <div className="d-flex justify-center align-center">
-            <div>
-              <img src={require("../../images/subScript.svg").default} alt="" />
+            <p className="white-color">{movie.overview}</p> 
+            <div className="d-flex justify-evenly align-center gap-2">
+              {buttonContent && <button onClick={toggleHeight}>{buttonContent}</button>} 
+              
             </div>
-            <p className="white-color font-12"> زیرنویس </p>
-          </div>
-          <div className="d-flex justify-center align-center">
-            <p className="white-color font-12">
-              فیلم - {movie.release_date ? movie.release_date.substring(0, 4) : "Release Date Not Available"}
-            </p>
+            <Link to="/" className="white-bgc d-flex flex-row-reverse align-center border-radius-5"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" class="Component-icon-0-1-456"><path d="M14.66 8.347l-8.537-4.93c-.502-.287-1.12-.286-1.62.005s-.81.825-.81 1.404v9.858c-.001.58.307 1.115.808 1.406s1.12.293 1.62.005l8.537-4.93a1.64 1.64 0 0 0 0-2.82z"></path></svg><span className="">خرید اشتراک</span></Link>
           </div>
         </div>
-        <p>{/* Render additional content */}</p>
-        <div className="d-flex justify-evenly align-center gap-2">
-          <button ref={buttonRef} onClick={toggleHeight}>Toggle Height</button>
-          {/* Render additional content */}
-        </div>
-      </div>
+      )}
     </div>
   );
 };
--- src/components/SliderMovieHomePage/index.jsx
+++ src/components/SliderMovieHomePage/index.jsx
@@ -9,84 +9,92 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import { Keyboard, Navigation } from "swiper/modules";
 import ratingDecimal from "../../helpers/ratingdecimal";
-import "./index.css"
+import "./index.css";
 import MovieInfoHomePage from "../MovieInfoHomePage";
 
 const apiKey = '4fba95dbf46cd77d415830c228c9ef01';
 
+export default function SliderMovie({ genreId, title }) {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const [selectedMovie, setSelectedMovie] = useState(null);
+  const [isInfoVisible, setIsInfoVisible] = useState(false); // State to control visibility of movie info
 
-
-export default function SliderMovie({ genreId , title }) {
-    const [loading, setLoading] = useState(true);
-    const [movies, setMovies] = useState([]);
-    const [selectedMovie, setSelectedMovie] = useState(null);
-
-    useEffect(() => {
-        async function fetchMovies() {
-            try {
-                const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=${genreId}`);
-                if (!response.ok) {
-                    throw new Error('Failed to fetch movies');
-                }
-                const data = await response.json();
-                setMovies(data.results || []);
-                setLoading(false);
-            } catch (error) {
-                console.error('Error fetching movies:', error);
-            }
+  useEffect(() => {
+    async function fetchMovies() {
+      try {
+        const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=${genreId}`);
+        if (!response.ok) {
+          throw new Error('Failed to fetch movies');
         }
-        fetchMovies();
-    }, [genreId]);    
+        const data = await response.json();
+        setMovies(data.results || []);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching movies:', error);
+      }
+    }
+    fetchMovies();
+  }, [genreId]);
 
-    const handleMovieSelect = (movie) => {
-        setSelectedMovie(movie);
-    };
+  const handleMovieSelect = (movie) => {
+    setSelectedMovie(movie);
+    setIsInfoVisible(true); // Show movie info when a movie is selected
+  };
 
-    return (
-        <div className="container">
-            {loading ? (
-                <Loading />
-            ) : (
-                <div className="d-flex flex-column justify-center align-start gap-2">
-                    <h3 className="white-color">{title}</h3>
-                    <Swiper
-                        grabCursor={true}
-                        keyboard={{
-                            enabled: true,
-                        }}
-                        spaceBetween={30}
-                        navigation={true}
-                        slidesPerView={7}
-                        modules={[Navigation, Keyboard]}
-                        className="mySwiper SliderContainer col-12 d-flex flex-row justify-evenly align-center gap-4"
-                    >
-                        {movies.length > 0 ? (
-                            movies.map((movie) => (
-                                <SwiperSlide key={movie.id} className="movieSlider h-auto" onClick={() => handleMovieSelect(movie)}>
-                                    <div className="movieSliderLink d-flex flex-column gap-2 position-relative">
-                                        <div className="movieSliderItem w-100 h-100 position-relative z-0">
-                                            <img
-                                                className="w-100 h-100 border-radius-5 object-cover"
-                                                src={movie.poster_path ? `${img_300}/${movie.poster_path}` : unavailable}
-                                                alt={movie.title}
-                                            />
-                                            <div className="darkMovieCover position-absolute z-1 top-0 right-0 w-100 h-100 d-flex flex-column justify-end align-start gap-1 border-radius-5">
-                                                <div className="d-flex justify-center align-center"><div><img src={require("../../images/IMDB.svg").default} alt=""/></div><p className="white-color font-14">{ratingDecimal(movie.vote_average)}</p></div>
-                                                <div className="d-flex justify-center align-center"><div><img src={require("../../images/subScript.svg").default} alt=""/></div><p className="white-color font-12"> زیرنویس </p></div>
-                                                <div className="d-flex justify-center align-center"><p className="white-color font-12">فیلم - {movie.release_date.substring(0,4)}</p></div>
-                                            </div>
-                                        </div>
-                                        <h5 className="white-color">{movie.title}</h5>
-                                    </div>
-                                </SwiperSlide>
-                            ))
-                        ) : (
-                            <p>No movies available for this genre</p>
-                        )}
-                    </Swiper>
-                </div>
-            )}
-            {selectedMovie && <MovieInfoHomePage movie={selectedMovie} />}
-        </div>
-    );
+  const handleInfoToggle = () => {
+    setIsInfoVisible(false); // Hide movie info when the button is clicked
+  };
+
+  return (
+    <div className="container">
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <div className="d-flex flex-column justify-center align-start gap-2">
+            <h3 className="white-color">{title}</h3>
+            <Swiper
+              grabCursor={true}
+              keyboard={{
+                enabled: true,
+              }}
+              spaceBetween={30}
+              navigation={true}
+              slidesPerView={7}
+              modules={[Navigation, Keyboard]}
+              className="mySwiper SliderContainer col-12 d-flex flex-row justify-evenly align-center gap-4"
+            >
+              {movies.length > 0 ? (
+                movies.map((movie) => (
+                  <SwiperSlide key={movie.id} className="movieSlider h-auto" onClick={() => handleMovieSelect(movie)}>
+                    <div className="movieSliderLink d-flex flex-column gap-2 position-relative">
+                      <div className="movieSliderItem w-100 h-100 position-relative z-0">
+                        <img
+                          className="w-100 h-100 border-radius-5 object-cover"
+                          src={movie.poster_path ? `${img_300}/${movie.poster_path}` : unavailable}
+                          alt={movie.title}
+                        />
+                        <div className="darkMovieCover position-absolute z-1 top-0 right-0 w-100 h-100 d-flex flex-column justify-end align-start gap-1 border-radius-5">
+                          <div className="d-flex justify-center align-center"><div><img src={require("../../images/IMDB.svg").default} alt=""/></div><p className="white-color font-14">{ratingDecimal(movie.vote_average)}</p></div>
+                          <div className="d-flex justify-center align-center"><div><img src={require("../../images/subScript.svg").default} alt=""/></div><p className="white-color font-12"> زیرنویس </p></div>
+                          <div className="d-flex justify-center align-center"><p className="white-color font-12">فیلم - {movie.release_date.substring(0,4)}</p></div>
+                        </div>
+                      </div>
+                      <h5 className="white-color">{movie.title}</h5>
+                    </div>
+                  </SwiperSlide>
+                ))
+              ) : (
+                <p>No movies available for this genre</p>
+              )}
+            </Swiper>
+          </div>
+          {/* Render MovieInfoHomePage only if a movie is selected, isInfoVisible is true, and selectedMovie is not null */}
+          {selectedMovie && isInfoVisible && <MovieInfoHomePage movie={selectedMovie} onToggle={handleInfoToggle} buttonContent="Custom Button" />}
+        </>
+      )}
+    </div>
+  );
 };
+
